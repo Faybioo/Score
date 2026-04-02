80ea9ffd25d4d8b73da46c6104792d81437d119b
ferns5 <granttirrell0310@gmail.com>
2026-04-02 19:14:18 -0400

    revised match json object file

--- api/main.go
+++ api/main.go
@@ -35,7 +35,7 @@ func main() {
     }
 
 	//sync json match data
-	const gistURL = "https://gist.githubusercontent.com/ferns5/1b90d98ce188fbc68cebc3e731be7d9c/raw/ae643158c1b6090380894cca89b86653e557a843/matches.json"
+	const gistURL = "https://gist.githubusercontent.com/ferns5/1b90d98ce188fbc68cebc3e731be7d9c/raw/b6550ea5a71c3b027e9f41c3e365f7c818efffb0/matches.json"	
 	go func() {
 		fmt.Println("Synchronizing match data...")
 		if err := services.SyncMatchesFromGist(db, gistURL); err != nil {
